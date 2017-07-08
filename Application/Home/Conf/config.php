f3831d749bf09c23ccb26cb162ae3aa1b7a119d4
letsread <13375710847@163.com>
2017-07-08 14:24:27 +0800

    加了一点注释+白依卓4个网页加入
    创建了news目录，并把白白依卓的网页加入到了news目录下。

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_TYPE' => 'mysql',       //数据库类型
-    'DB_HOST' => 'localhost',   //host地址
+    'DB_HOST' => '127.0.0.1',   //host地址
     'DB_USER' => 'root',        //数据库用户名
     'DB_PWD' => 'root',         //数据库密码
     'DB_NAME' => 'letsread',  //数据名
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,19 +3,29 @@ namespace Home\Controller;
 use Think\Controller;
 
 
-
 class IndexController extends Controller
 {
+    /***
+     * 程序入口
+     */
+    public function index()
+    {
+        $this->display();
+    }
+
+    /***
+     * 检验用户名密码是否正确
+     * @param $username
+     * @param $password
+     * @return
+     */
     public function login()
     {
-        /*
-         * 检验用户名密码是否正确
-         */
         $username = I("username","trim");   //接收用户名并用trim函数除去首尾空格
         $password = I("password", "md5");   //接收密码并加密（数据库中是加密后的密码）
 
         $map['username'] = $username;
-        $admin = M('stu')->where($map)->find(); //相当于select * from lsr_stu where username = '$username' limit 1;
+        $admin = M('user')->where($map)->find(); //相当于select * from lsr_stu where username = '$username' limit 1;
         if($admin['password'] === $password)
         {
             echo '登录成功';
