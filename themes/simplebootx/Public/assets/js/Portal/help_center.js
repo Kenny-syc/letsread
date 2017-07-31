b81c385bf24f894228066af1e235bba2f2014d84
letsread <13375710847@163.com>
2017-07-31 17:33:56 +0800

    帮助中心前台完成
    完成帮助中心-。-

---
+++ themes/simplebootx/Public/assets/js/Portal/help_center.js
@@ -0,0 +1,147 @@
+/**
+ * Created by baiyizhuo on 2017/7/30.
+ */
+function removeElement1(a,b)
+{
+    a.style.display="none";
+    a1.style.display="block";
+    hideId1=a1;
+    b.style.color="#575654";
+    b1.style.color="#e16781";
+    resumeColor1=b1;
+}
+function removeElement2(a,b)
+{
+    a.style.display="none";
+    a2.style.display="block";
+    hideId1=a2;
+    b.style.color="#575654";
+    b2.style.color="#e16781";
+    resumeColor1=b2;
+}
+function removeElement3(a,b)
+{
+    a.style.display="none";
+    a3.style.display="block";
+    hideId1=a3;
+    b.style.color="#575654";
+    b3.style.color="#e16781";
+    resumeColor1=b3;
+}
+function removeElement4(a,b)
+{
+    a.style.display="none";
+    a4.style.display="block";
+    hideId1=a4;
+    b.style.color="#575654";
+    b4.style.color="#e16781";
+    resumeColor1=b4;
+}
+function removeElement5(a,b)
+{
+    a.style.display="none";
+    a5.style.display="block";
+    hideId1=a5;
+    b.style.color="#575654";
+    b5.style.color="#e16781";
+    resumeColor1=b5;
+}
+function removeElement6(a,b)
+{
+    a.style.display="none";
+    a6.style.display="block";
+    hideId1=a6;
+    b.style.color="#575654";
+    b6.style.color="#e16781";
+    resumeColor1=b6;
+}
+function removeElement7(a,b)
+{
+    a.style.display="none";
+    a7.style.display="block";
+    hideId1=a7;
+    b.style.color="#575654";
+    b7.style.color="#e16781";
+    resumeColor1=b7;
+}
+function chooseTeacher()
+{
+    document.getElementById("area2").style.display="none";
+    document.getElementById("area1").style.display="block";
+    document.getElementById("teacherChoose").style.color="#ee745d";
+    document.getElementById("studentChoose").style.color="#585657"
+    hideId2.style.display="none";
+    hideId1.style.display="block";
+}
+function chooseStudent()
+{
+    document.getElementById("area1").style.display="none";
+    document.getElementById("area2").style.display="block";
+    document.getElementById("teacherChoose").style.color="#585657";
+    document.getElementById("studentChoose").style.color="#ee745d"
+    hideId1.style.display="none";
+    hideId2.style.display="block";
+}
+function removeElement11(a,b)
+{
+
+    a.style.display="none";
+    a11.style.display="block";
+    hideId2=a11;
+    b.style.color="#575654";
+    b11.style.color="#e16781";
+    resumeColor2=b11;
+}
+function removeElement22(a,b)
+{
+    a.style.display="none";
+    a22.style.display="block";
+    hideId2=a22;
+    b.style.color="#575654";
+    b22.style.color="#e16781";
+    resumeColor2=b22;
+}
+function removeElement33(a,b)
+{
+    a.style.display="none";
+    a33.style.display="block";
+    hideId2=a33;
+    b.style.color="#575654";
+    b33.style.color="#e16781";
+    resumeColor2=b33;
+}
+function removeElement44(a,b)
+{
+    a.style.display="none";
+    a44.style.display="block";
+    hideId2=a44;b.style.color="#575654";
+    b44.style.color="#e16781";
+    resumeColor2=b44;
+}
+function removeElement55(a,b)
+{
+    a.style.display="none";
+    a55.style.display="block";
+    hideId2=a55;
+    b.style.color="#575654";
+    b55.style.color="#e16781";
+    resumeColor2=b55;
+}
+function removeElement66(a,b)
+{
+    a.style.display="none";
+    a66.style.display="block";
+    hideId2=a66;
+    b.style.color="#575654";
+    b66.style.color="#e16781";
+    resumeColor2=b66;
+}
+function removeElement77(a,b)
+{
+    a.style.display="none";
+    a77.style.display="block";
+    hideId2=a77;
+    b.style.color="#575654";
+    b77.style.color="#e16781";
+    resumeColor2=b77;
+}
